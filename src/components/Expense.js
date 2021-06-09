fea9f5270ce47a26692754e3a50f3855497c72fb
Edward Ascan <edward.ascan@me.com>
2021-06-09 11:17:03 -0400

    install react hook forms, work on stlying expense page

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -1,15 +1,25 @@
 import "./Expense.css";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import {IconButton} from "@material-ui/core";
+import {useForm} from "react-hook-form";
+import {useHistory} from "react-router";
 
 function Expense() {
+  const {register, handleSubmit, watch, serrors} = useForm();
+
+  const history = useHistory();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="expense">
       <div className="expense__top">
-        <IconButton>
-          <ArrowBackIcon />
+        <IconButton className="back__button">
+          <ArrowBackIcon onClick={() => history.push("/")} />
         </IconButton>
-        <h3>Add an expense</h3>
+        <h3 className="expense__topText">Add an expense</h3>
       </div>
       <div className="expense__form"></div>
     </div>
